614e2af7e5c1844cddfdbc848fe3ceae302cf280
briup <@briup>
2021-10-06 17:10:09 +0800

    hot-fix commit

--- src/main/java/com/briup/Javatest.java
+++ src/main/java/com/briup/Javatest.java
@@ -8,6 +8,7 @@ public class Javatest {
         System.out.println("hello");
         System.out.println("hello2");
         System.out.println("hello3");
+        System.out.println("hello4");
 
 
     }
