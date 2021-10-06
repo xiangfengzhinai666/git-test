6fd15765852cbef6b091b6fa68f87dfe69d7d9a0
briup <@briup>
2021-10-06 16:58:09 +0800

    second commit

--- src/main/java/com/briup/Javatest.java
+++ src/main/java/com/briup/Javatest.java
@@ -1,7 +1,12 @@
 package com.briup;
 
+/**
+ * 我这个添加的时候没有弹窗告诉是否需要忽略啥的，不知道是git.ignore有问题 ，还是直接帮我忽略了
+ */
 public class Javatest {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello2");
+
     }
 }
